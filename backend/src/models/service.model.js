07b561e2535ed9c125a8341ddab095cb2ec95880
Leo <takib77@gmail.com>
2021-08-09 14:03:00 +0200

    1. feladat

--- backend/src/models/service.model.js
+++ backend/src/models/service.model.js
@@ -1,1 +1,35 @@
 // 1. Feladat: pótold a service modellt.
+const mongoose = require('mongoose');
+
+const ServiceSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    since: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    street: {
+        type: String,
+        required: false
+    },
+    city: {
+        type: String,
+        required: false
+    },
+    country: {
+        type: String,
+        required: false
+    },
+    active: {
+        type: Boolean,
+        required: false,
+        default: true
+    }
+}, {
+    timeStamps: true
+});
+
+module.exports = mongoose.model('Service', ServiceSchema);
